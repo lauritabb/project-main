6b3ff47ec382c39827106e5d1e3b8787831b29ea
lauritabb <lauritabenitez@gmail.com>
2019-05-28 14:16:24 -0700

    add functions to song.service

--- login-client/src/app/song.service.ts
+++ login-client/src/app/song.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, from} from 'rxjs';
 import { Router } from '@angular/router';
+import { Song } from './song'
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,12 @@ export class SongService {
   baseURL = 'https://localhost:800/song'
   constructor(private http:HttpClient, private route:Router) { }
 
-  addSong(sonData:object){}
+  addSong(){}
+
+  getOneSong(user_id): Observable<Song>{
+    return this.http.get<Song>(this.baseURL + '/'+user_id);
+  }
+  getSongs(): Observable<Song[]>{
+    return this.http.get<Song[]>(this.baseURL);
+  }
 }
