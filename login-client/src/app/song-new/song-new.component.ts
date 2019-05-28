a948bc39b70606e1f954c747468121b5286773b6
lauritabb <lauritabenitez@gmail.com>
2019-05-28 13:48:17 -0700

    created form for adding new song

--- login-client/src/app/song-new/song-new.component.ts
+++ login-client/src/app/song-new/song-new.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./song-new.component.css']
 })
 export class SongNewComponent implements OnInit {
-
+ createForm: object ={
+    title:'',
+    artist:'',
+  }
   constructor() { }
 
   ngOnInit() {
